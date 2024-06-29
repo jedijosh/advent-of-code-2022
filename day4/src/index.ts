fe3ed79b530a94ecdc02e2a389b5b5356fbadd93
Josh Sikorski <josh.sikorski@gmail.com>
2024-06-29 18:00:14 -0500

    Solved day 4

--- day4/src/index.ts
+++ day4/src/index.ts
@@ -1,5 +1,5 @@
 import { parseFileIntoArrayOfLines } from '../../utils'
-const dataFolder = '/mnt/c/Users/joshs/code/advent-of-code-2022-data/day3/data'
+const dataFolder = '/mnt/c/Users/joshs/code/advent-of-code-2022-data/day4/data'
 
 const LOGGING = true
 
@@ -9,36 +9,19 @@ export async function solvePartOne ( filename : string) {
         
     let totalSum: number = 0
     for (let lineNumber = 0; lineNumber < fileLines.length; lineNumber++) {
-        let line: string = fileLines[lineNumber]
-        let firstHalf: string[] = line.slice(0, line.length / 2).split('')
+        let elfAssignments = fileLines[lineNumber].split(',')
+        const firstElfAssignment = elfAssignments[0].split('-')
+        const secondElfAssignment = elfAssignments[1].split('-')
+        console.log('')
 
-        // De-duplicate the array to speed up comparison
-        let uniqueFirstHalf: string[] = firstHalf.filter((item, pos) => {
-            return firstHalf.indexOf(item) === pos
-        })
-        let secondHalf: string[] = line.slice(line.length / 2).split('')
-        // De-duplicate the array to speed up comparison
-        let uniqueSecondHalf: string[] = secondHalf.filter((item, pos) => {
-            return secondHalf.indexOf(item) === pos
-        })
-        let commonItem: string = ''
-        for (let i = 0; i < uniqueFirstHalf.length; i++) {
-            if (uniqueSecondHalf.indexOf(uniqueFirstHalf[i]) !== -1) {
-                commonItem = uniqueFirstHalf[i]
-                break
-            }
+        // Check if the first elf assignment is fully within the second
+        if (Number(firstElfAssignment[0]) >= Number(secondElfAssignment[0]) && Number(firstElfAssignment[1]) <= Number(secondElfAssignment[1])) {
+            totalSum++
+        } else if (Number(secondElfAssignment[0]) >= Number(firstElfAssignment[0]) && Number(secondElfAssignment[1]) <= Number(firstElfAssignment[1])) {
+            totalSum++
         }
-
-        let priorityValue = commonItem.codePointAt(0) || 0
-        // Lowercase item types a through z have priorities 1 through 26.
-        // Uppercase item types A through Z have priorities 27 through 52.
-        if (priorityValue > 96) {
-            priorityValue -= 96
-        } else {
-            priorityValue -= 38
-        }
-        totalSum += priorityValue
     }
+    
     return totalSum
 }
 
@@ -47,53 +30,31 @@ export async function solvePartTwo ( filename : string) {
     let fileLines : string[] = await parseFileIntoArrayOfLines(filename)
         
     let totalSum: number = 0
+    for (let lineNumber = 0; lineNumber < fileLines.length; lineNumber++) {
+        let elfAssignments = fileLines[lineNumber].split(',')
+        const firstElfAssignment = elfAssignments[0].split('-')
+        const secondElfAssignment = elfAssignments[1].split('-')
+        const firstAssignmentStart = Number(firstElfAssignment[0])
+        const firstAssignmentEnd = Number(firstElfAssignment[1])
+        const secondAssignmentStart = Number(secondElfAssignment[0])
+        const secondAssignmentEnd = Number(secondElfAssignment[1])
 
-    // Elves are in groups of 3.  There is one common letter amongst the 3 elves
-    for (let lineNumber = 0; lineNumber < fileLines.length; lineNumber = lineNumber + 3) {
-        let line: string = fileLines[lineNumber]
-        let firstElf: string[] = fileLines[lineNumber].split('')
-        let secondElf: string[] = fileLines[lineNumber + 1].split('')
-        let thirdElf: string[] = fileLines[lineNumber + 2].split('')
-
-        // De-duplicate the array to speed up comparison
-        let uniqueFirstElf: string[] = firstElf.filter((item, pos) => {
-            return firstElf.indexOf(item) === pos
-        })
-        
-        // De-duplicate the array to speed up comparison
-        let uniqueSecondElf: string[] = secondElf.filter((item, pos) => {
-            return secondElf.indexOf(item) === pos
-        })
-
-        // De-duplicate the array to speed up comparison
-        let uniqueThirdElf: string[] = thirdElf.filter((item, pos) => {
-            return thirdElf.indexOf(item) === pos
-        })
-
-        let commonBetweenFirstAndSecond: string[] = uniqueFirstElf.filter((item) => {
-            return uniqueSecondElf.indexOf(item) !== -1
-        })
-        
-        let commonItem: string = commonBetweenFirstAndSecond.filter((item) => {
-            return uniqueThirdElf.indexOf(item) !== -1
-        })[0]
-        
-        // Lowercase item types a through z have priorities 1 through 26.
-        // Uppercase item types A through Z have priorities 27 through 52.
-        let priorityValue = commonItem.codePointAt(0) || 0
-        if (priorityValue > 96) {
-            priorityValue -= 96
-        } else {
-            priorityValue -= 38
+        if (firstAssignmentStart >= secondAssignmentStart && firstAssignmentStart <= secondAssignmentEnd) {
+            totalSum++
+        } else if (secondAssignmentStart >= firstAssignmentStart && secondAssignmentStart <= firstAssignmentEnd) {
+            totalSum++
+        }  else if (firstAssignmentEnd <= secondAssignmentStart && firstAssignmentEnd >= secondAssignmentEnd) {
+            totalSum++
+        }  else if (secondAssignmentEnd <= firstAssignmentStart && secondAssignmentEnd >= firstAssignmentEnd) {
+            totalSum++
         }
-        totalSum += priorityValue
     }
     return totalSum
 }
 
-solvePartOne(dataFolder + '/tests/input.txt')
+// solvePartOne(dataFolder + '/tests/input.txt')
 // solvePartOne(dataFolder + '/input.txt')
 
 // solvePartTwo(dataFolder + '/tests/input.txt')
-// solvePartTwo(dataFolder + '/input.txt')
+solvePartTwo(dataFolder + '/input.txt')
     .then(answer => console.log('answer:', answer))
--- day4/tests/index.test.ts
+++ day4/tests/index.test.ts
@@ -8,18 +8,18 @@ describe('test solvePartOne', () => {
         expect(partOneAnswer).toBe(2)
     })
     
-    // test('result should be 8394 with my data', async () => {
-    //     let partOneAnswer = await solvePartOne(dataFolder + '/input.txt')
-    //     expect(partOneAnswer).toBe(8394)
-    // })
+    test('result should be 582 with my data', async () => {
+        let partOneAnswer = await solvePartOne(dataFolder + '/input.txt')
+        expect(partOneAnswer).toBe(582)
+    })
     
-    // test('part 2 result should be 70 with sample data', async () => {
-    //     let partTwoAnswer = await solvePartTwo(dataFolder + '/tests/input.txt')
-    //     expect(partTwoAnswer).toBe(70)
-    // })
+    test('part 2 result should be 4 with sample data', async () => {
+        let partTwoAnswer = await solvePartTwo(dataFolder + '/tests/input.txt')
+        expect(partTwoAnswer).toBe(4)
+    })
     
-    // test('part 2 result should be 2413 with my data', async () => {
-    //     let partTwoAnswer = await solvePartTwo(dataFolder + '/input.txt')
-    //     expect(partTwoAnswer).toBe(2413)
-    // })
+    test('part 2 result should be 893 with my data', async () => {
+        let partTwoAnswer = await solvePartTwo(dataFolder + '/input.txt')
+        expect(partTwoAnswer).toBe(893)
+    })
 })
